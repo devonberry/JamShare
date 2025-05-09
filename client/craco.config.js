5412d68be4941f86cc2dc82ce6b1c6faa29d4af3
devonberry <67202514+devonberry@users.noreply.github.com>
2025-05-09 10:52:37 -0700

    test

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,20 +1,23 @@
+const fs = require('fs');
+
 module.exports = {
-  /*
+
     devServer: {
-        
+
+        /*
         https: {
 
             //key: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/privkey.pem'),
             //cert: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
 
-            key: fs.readFileSync("../privkey.pem"),
-            cert: fs.readFileSync("../fullchain.pem"),
-
+            key: fs.readFileSync("//wsl.localhost/Ubuntu/home/thund/.local/share/mkcert/rootCA-key.pem"),
+            cert: fs.readFileSync("//wsl.localhost/Ubuntu/home/thund/.local/share/mkcert/rootCA.pem"),
         },
-        
-        host: "berryhousehold.ddns.net",
+        */
+
+        host: "jamshare.ddns.net",
         //host: "192.168.1.11",
-        port: 3000,
+        port: 80,
     }
-    */
+
 };
