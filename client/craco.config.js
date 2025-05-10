c4647f8b3da02572296f1d124ee333ce275577b3
devonberry <devonberry4@yahoo.com>
2025-05-09 17:13:19 -0700

    changed config

--- client/craco.config.js
+++ client/craco.config.js
@@ -4,16 +4,17 @@ module.exports = {
 
     devServer: {
 
-        /*
         https: {
 
             //key: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/privkey.pem'),
             //cert: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
 
-            key: fs.readFileSync("//wsl.localhost/Ubuntu/home/thund/.local/share/mkcert/rootCA-key.pem"),
-            cert: fs.readFileSync("//wsl.localhost/Ubuntu/home/thund/.local/share/mkcert/rootCA.pem"),
+            key: fs.readFileSync("/etc/letsencrypt/live/jamshare.ddns.net/fullchain.pem"),
+            cert: fs.readFileSync("/etc/letsencrypt/live/jamshare.ddns.net/privkey.pem"),
+            ca: fs.readFileSync('/etc/letsencrypt/live/jamshare.ddns.net/chain.pem'),
         },
-        */
+
+
 
         host: "jamshare.ddns.net",
         //host: "192.168.1.11",
